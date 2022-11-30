0da45180c3c3119da63222a07090a5bbb2f80034
Bladesheng <keadr23@gmail.com>
2022-11-30 19:54:34 +0100

    Add index view

--- controllers/gameController.ts
+++ controllers/gameController.ts
@@ -1,6 +1,6 @@
 import async from "async";
+import { Request, Response, NextFunction } from "express";
 import { body, validationResult } from "express-validator";
-import { Request, Response } from "express";
 
 import { Developer } from "../models/developer";
 import { Game } from "../models/game";
@@ -8,15 +8,25 @@ import { Genre } from "../models/genre";
 import { Tag } from "../models/tag";
 
 export const gameController = (() => {
-  function index(req: Request, res: Response) {
-    res.send("omg GAMECONTROLLER hi!!!");
-  }
+  function index(req: Request, res: Response, next: NextFunction) {
+    async.parallel(
+      {
+        gamesList(callback) {
+          Game.find({}, "name rating").exec(callback);
+        }
+      },
+      (err, results) => {
+        if (err) {
+          return next(err);
+        }
 
-  // app.get("/", (req: Request, res: Response) => {
-  //   res.render("pages/index", {
-  //     messages: messages
-  //   });
-  // });
+        res.render("pages/index", {
+          title: "Home",
+          games: results.gamesList
+        });
+      }
+    );
+  }
 
   return { index };
 })();
